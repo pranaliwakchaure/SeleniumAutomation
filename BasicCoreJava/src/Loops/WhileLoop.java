2d11067db104941cc62d66b13e1c365b49b748f4
pranaliwakchaure <pranaliwakchaure9@gmail.com>
2019-08-07 21:48:52 +0530

    commit

---
+++ BasicCoreJava/src/Loops/WhileLoop.java
@@ -0,0 +1,24 @@
+package Loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Initialization
+		int i=0;
+		
+		//condition
+		
+		while(i<10)
+		{
+			
+			System.out.println(i);
+			
+			//Increment
+			i++;
+		}
+
+	}
+
+}
