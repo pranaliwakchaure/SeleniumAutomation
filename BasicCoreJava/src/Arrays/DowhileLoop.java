bd7a2417978d8351828027c715c13afb8cf4d07a
pranaliwakchaure <pranaliwakchaure9@gmail.com>
2019-08-07 21:48:21 +0530

    commit

---
+++ BasicCoreJava/src/Arrays/DowhileLoop.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class DowhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=10;
+		do {
+			System.out.println(i);
+			i++;
+		}while(i<10);
+		
+		while(i<10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+
+}
