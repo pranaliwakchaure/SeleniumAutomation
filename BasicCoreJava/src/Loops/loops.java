eb6ba95b12d0713d7510a95e7383f87b7287b539
pranaliwakchaure <pranaliwakchaure9@gmail.com>
2019-08-07 21:48:39 +0530

    commit

---
+++ BasicCoreJava/src/Loops/loops.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean login=false;
+		
+		
+		for (int i=0;  i<=10;  i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
