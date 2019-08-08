9fe9d6ee7e9135fa0a8a955809b465ab464cc274
pranaliwakchaure <pranaliwakchaure9@gmail.com>
2019-08-08 21:47:43 +0530

    commit

---
+++ BasicCoreJava/src/Functions/Car.java
@@ -0,0 +1,13 @@
+package Functions;
+
+public class Car 
+{
+  static int wheels=4;
+  String colour="White";
+  String model="VDI";
+  
+  public void braking()
+  {
+	  System.out.println(colour +" "+ model +" is having "+ wheels +"  wheels");
+  }
+}
