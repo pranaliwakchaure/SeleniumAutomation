f91a8116bbcdd9dc6d561280b41f3279477bcc8a
pranaliwakchaure <pranaliwakchaure9@gmail.com>
2019-08-08 21:47:55 +0530

    commit

---
+++ BasicCoreJava/src/Functions/function2.java
@@ -0,0 +1,17 @@
+package Functions;
+
+public class function2 {
+
+	public static void main(String[] args)
+	{
+		String str=Myname("Pranali","Wakchaure");
+		System.out.println(str);
+
+	}
+	
+	public static String Myname(String name,String surname)
+	{
+		return "Your name is " +name+ " "+surname;
+	}
+
+}
