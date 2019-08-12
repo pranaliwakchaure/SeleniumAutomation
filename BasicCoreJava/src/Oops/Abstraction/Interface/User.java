766c5c9c8227b179ca70319bf867c0cc9e520519
pranaliwakchaure <pranaliwakchaure9@gmail.com>
2019-08-12 21:34:18 +0530

    commit

--- BasicCoreJava/src/Oops/Abstraction/Interface/User.java
+++ BasicCoreJava/src/Oops/Abstraction/Interface/User.java
@@ -5,26 +5,44 @@ public class User {
 	public static void main(String[] args) 
 	{
 	
-		RBI obj=new SBI();
-		obj.CurrentAccount();
-		obj.SavingAccount();
-		obj.debitcard();
-		obj.creaditcard();
+		
+	
+		RBI bank=null;
+		String bankname;
 		
 		
-		RBI obj1=new IDBI();
-		obj1.CurrentAccount();
-		obj1.SavingAccount();
-		obj1.debitcard();
-		obj1.creaditcard();
+		//bankname="SBI";
+		bankname="IDBI";
+		//bankname="CitiBank";
 		
-		RBI obj2=new CitiBank();
-		obj2.CurrentAccount();
-		obj2.SavingAccount();
-		obj2.debitcard();
-		obj2.creaditcard();
+		if(bankname.equals("SBI"))
+		{
+			bank=new SBI();
+		}else if(bankname.equals("IDBI"))
+		{
+			bank=new IDBI();
+		}else if(bankname.equals("CitiBank"))
+		{
+			bank=new CitiBank();
+		}
 		
-
+		
+		bank.CurrentAccount();
+		bank.SavingAccount();
+		bank.debitcard();
+		bank.creaditcard();
+		
+		/*
+		 * RBI obj=new SBI(); obj.CurrentAccount(); obj.SavingAccount();
+		 * obj.debitcard(); obj.creaditcard();
+		 * 
+		 * RBI obj1=new IDBI(); obj1.CurrentAccount(); obj1.SavingAccount();
+		 * obj1.debitcard(); obj1.creaditcard();
+		 * 
+		 * RBI obj2=new CitiBank(); obj2.CurrentAccount(); obj2.SavingAccount();
+		 * obj2.debitcard(); obj2.creaditcard();
+		 * 
+		 */
 	}
 
 }
